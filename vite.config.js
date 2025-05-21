c8dbaa8f5d99d2b34532536a364ab95fff5a7964
shivam-python <raynoldw222@gmail.com>
2025-05-21 21:47:26 +0530

    about me section added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true, // allows external access (e.g. via ngrok)
-    origin: 'https://2638-106-222-217-24.ngrok-free.app', // optional: sets correct URL
+    origin: 'https://cool-ends-wink.loca.lt/', // optional: sets correct URL
   },
 });
